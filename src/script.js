2d690d7b0c108caa24ac91a34fb046925d200f32
pel0nhtml <sperez@fwdcostarica.com>
2024-07-24 08:34:56 -0600

    primercambio

---
+++ src/script.js
@@ -0,0 +1,8 @@
+const nombre = document.getElementById("nombre")
+const correo = document.getElementById("correo")
+const pass = document.getElementById("pass")
+const btnR = document.getElementById("btnRegi")
+
+const nombreI=document.getElementById("Identi")
+const passI = document.getElementById("passI")
+const btnI=document.getElementById ("btnIden")
