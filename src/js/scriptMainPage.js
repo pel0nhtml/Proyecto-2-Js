d9b0b3b7ac5df549adeb65a739e24154287c3717
pel0nhtml <sperez@fwdcostarica.com>
2024-07-25 12:44:42 -0600

    quintoArreglo

--- src/js/scriptMainPage.js
+++ src/js/scriptMainPage.js
@@ -1,49 +1,82 @@
-const textM =document.getElementById("textM");
-const btnC = document.getElementById("btnC");
 const container = document.getElementById("containerT");
-const btnU = document.getElementById("btnU");
+const listaTarea = document.getElementById("listaTarea");
+const listaEvento =document.getElementById("listaEvento");
+const selectah = document.getElementById("selectah");
+const DateR = document.getElementById("DateR");
 
-btnC.addEventListener("click", function () {
-    const containerN =document.createElement("div")
-    const pTag = document.createElement("p");
-    pTag.id ="TextoP"
-    const btnD = document.createElement("button");
-    const btnE = document.createElement("button");
-    containerN.appendChild(pTag);
-    containerN.appendChild(btnD);
-    containerN.appendChild(btnE);
-    pTag.innerHTML = textM.value;
-    btnD.innerHTML = "Delete Tasks";
-    btnE.innerHTML = "Edit";
-    let undoDelete = textM.value;
-    localStorage.setItem('recover',undoDelete);
-    containerT.appendChild(containerN);
+btnSave.addEventListener("click", function () {
+    const selection = selectah.value
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-if (btnD.addEventListener("click", function () {
-    containerT.removeChild(containerN);
-    alert("Deleted Succesfully");
-})) {}
+    ////////////////////////////tareas///////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
+    if (selection === "Task") {
+        const containerN =document.createElement("div")
+        const pTag = document.createElement("p");
+        pTag.id ="TextoP"
+        const btnD = document.createElement("button");
+        const btnE = document.createElement("button");
+        containerN.appendChild(pTag);
+        containerN.appendChild(btnD);
+        containerN.appendChild(btnE);
+        pTag.innerHTML = textM.value +" "+ DateR.value;
+        btnD.innerHTML = "Delete Tasks"; 
+        btnE.innerHTML = "Edit";
+        listaTarea.appendChild(containerN);
 
-if (btnE.addEventListener("click", function () {
-    const btnA = document.createElement("button");
-    containerN.appendChild(btnA);
-    btnA.innerHTML = "Apply";
-    pTag.contentEditable = true;
-    pTag.style.backgroundColor = "#666";
-    pTag.style.color = "white";
+        if (btnD.addEventListener("click", function () {
+            listaTarea.removeChild(containerN);
+            alert("Deleted Succesfully");
+        })) {}
+        
+        if (btnE.addEventListener("click", function () {
+            const btnA = document.createElement("button");
+            containerN.appendChild(btnA);
+            btnA.innerHTML = "Apply";
+            pTag.contentEditable = true;
+            pTag.style.backgroundColor = "#666";
+            pTag.style.color = "white";
+        
+        if (btnA.addEventListener("click", function () {
+            pTag.contentEditable = false;
+            pTag.style.backgroundColor = "White";
+            pTag.style.color = "Black";
+       /////////////////////////////////////////////////////////////////////////////////////////////////////
+       
+        })){}/////////////////Eventos///////////////////////////////
+        })){}
+     /////////////////////////////////////////////////////////////////////////////////////////////////////////////   
+    } else { if (selection === "Event") {
+        const containerN =document.createElement("div")
+        const pTag = document.createElement("p");
+        pTag.id ="TextoP"
+        const btnD = document.createElement("button");
+        const btnE = document.createElement("button");
+        containerN.appendChild(pTag);
+        containerN.appendChild(btnD);
+        containerN.appendChild(btnE);
+        pTag.innerHTML = textM.value +" "+ DateR.value;
+        btnD.innerHTML = "Delete Tasks";
+        btnE.innerHTML = "Edit";
+        listaEvento.appendChild(containerN);
 
-if (btnA.addEventListener("click", function () {
-    pTag.contentEditable = false;
-    pTag.style.backgroundColor = "White";
-    pTag.style.color = "Black";
-
-if (btnU.addEventListener("click",function () {
-    let taskDeleted =localStorage.getItem('recover')
-    TextoP.innerHTML= taskDeleted
-
-
-})) {}
-
-})){}
-})){}
-})
+        if (btnD.addEventListener("click", function () {
+            listaEvento.removeChild(containerN);
+            alert("Deleted Succesfully");
+        })) {}
+        
+        if (btnE.addEventListener("click", function () {
+            const btnA = document.createElement("button");
+            containerN.appendChild(btnA);
+            btnA.innerHTML = "Apply";
+            pTag.contentEditable = true;
+            pTag.style.backgroundColor = "#666";
+            pTag.style.color = "white";
+        
+        if (btnA.addEventListener("click", function () {
+            pTag.contentEditable = false;
+            pTag.style.backgroundColor = "White";
+            pTag.style.color = "Black";
+        })){}
+        })){}
+    } }})
