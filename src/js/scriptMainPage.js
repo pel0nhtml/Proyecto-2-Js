6397ff08c1985a01ac9915101b90358084271578
pel0nhtml <sperez@fwdcostarica.com>
2024-07-29 08:25:34 -0600

    setimoArreglo

--- src/js/scriptMainPage.js
+++ src/js/scriptMainPage.js
@@ -1,102 +1,107 @@
 const container = document.getElementById("containerT");
-const tareaEvent = document.getElementById ("tareaEvent")
-const textM = document.getElementById("listaTarea");
-const listaEvento =document.getElementById("listaEvento");
+const tareaEvent = document.getElementById("tareaEvent");
+const listaTarea = document.getElementById("listaTarea");
+const listaEvento = document.getElementById("listaEvento");
 const selectah = document.getElementById("selectah");
 const DateR = document.getElementById("DateR");
+const btnSave = document.getElementById("btnSave");
 
+let taskLista = JSON.parse(localStorage.getItem("taskInfo")) || [];
+let eventLista = JSON.parse(localStorage.getItem("eventInfo")) || [];
 
-let taskLista= []
-let eventLista= []
+///Función para renderizar tareas y eventos al cargar la página///
+function renderList() {
+    listaTarea.innerHTML = "<h2>Tareas</h2>";
+    listaEvento.innerHTML = "<h2>Eventos</h2>";
+    taskLista. forEach ((tsk, index) => renderItem (tsk, index, "Task"));
+    eventLista. forEach ((evnt, index) => renderItem (evnt, index, "Event"));
+}
+
+///Función para crear y mostrar un nuevo ítem (tarea o evento) en el DOM///
+function renderItem(item, index, type) {
+    const containerN = document.createElement("div");
+    const pTag = document.createElement("p");
+    pTag.id = "TextoP";
+    const btnD = document.createElement("button");
+    const btnE = document.createElement("button");
+
+    containerN.appendChild(pTag);
+    containerN.appendChild(btnD);
+    containerN.appendChild(btnE);
+
+    pTag.innerHTML = item;
+
+    btnD.innerHTML = "Delete";
+    btnE.innerHTML = "Edit";
+
+    if (type === "Task") {
+        listaTarea.appendChild(containerN);
+        ///Eliminar tarea///
+        btnD.addEventListener("click", () => deleteItem(index, "Task", containerN));
+        ///Editar tarea///
+        btnE.addEventListener("click", () => editItem(index, "Task", pTag));
+    } else {
+        listaEvento.appendChild(containerN);
+        ///Eliminar evento///
+        btnD.addEventListener("click", () => deleteItem(index, "Event", containerN));
+        ///Editar evento///
+        btnE.addEventListener("click", () => editItem(index, "Event", pTag));
+    }
+}
 
 btnSave.addEventListener("click", function () {
-    const selection = selectah.value
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    ////////////////////////////tareas///////////////////////////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////////////////////////////////////
+    const selection = selectah.value;
+    const taskEventValue = tareaEvent.value + " " + DateR.value;
+////////////////////////////tareas///////////////////////////////////////////////////////////////////////////////
     if (selection === "Task") {
-        const containerN =document.createElement("div")
-        const pTag = document.createElement("p");
-        pTag.id ="TextoP"
-        const btnD = document.createElement("button");
-        const btnE = document.createElement("button");
-        containerN.appendChild(pTag);
-        containerN.appendChild(btnD);
-        containerN.appendChild(btnE);
-       
-    //////////////////////
-        let tsk = tareaEvent.value +" "+ DateR.value;
-        pTag.innerHTML = tsk
-    /////////////////////
-       
-        btnD.innerHTML = "Delete Tasks"; 
-        btnE.innerHTML = "Edit";
-        listaTarea.appendChild(containerN);
+        taskLista.push(taskEventValue);
+        localStorage.setItem("taskInfo", JSON.stringify(taskLista));
+        renderItem(taskEventValue, taskLista.length - 1, "Task");
+////////////////////////////eventos///////////////////////////////////////////////////////////////////////////////
+    } else if (selection === "Event") {
+        eventLista.push(taskEventValue);
+        localStorage.setItem("eventInfo", JSON.stringify(eventLista));
+        renderItem(taskEventValue, eventLista.length - 1, "Event");
+    }
+});
 
+///Función para eliminar un ítem de la lista y del DOM, actualizando también el local storage///
+function deleteItem(index, type, element) {
+    if (type === "Task") {
+        taskLista.splice(index, 1);
+        localStorage.setItem("taskInfo", JSON.stringify(taskLista));
+        listaTarea.removeChild(element);
+    } else {
+        eventLista.splice(index, 1);
+        localStorage.setItem("eventInfo", JSON.stringify(eventLista));
+        listaEvento.removeChild(element);
+    }
+}
 
-    ////////////////////
-        taskLista.push (tsk);
-        localStorage.setItem ("taskInfo", JSON.stringify(taskLista))
-    ///////////////////
+///Función para editar un ítem, guardando los cambios en la lista y en el local storage///
+function editItem(index, type, pTag) {
+    const btnA = document.createElement("button");
+    btnA.innerHTML = "Apply";
+    pTag.contentEditable = true;
 
-        if (btnD.addEventListener("click", function () {
-            listaTarea.removeChild(containerN);
-            alert("Deleted Succesfully");
-        })) {}
-        
-        if (btnE.addEventListener("click", function () {
-            const btnA = document.createElement("button");
-            containerN.appendChild(btnA);
-            btnA.innerHTML = "Apply";
-            pTag.contentEditable = true;
-        
-        if (btnA.addEventListener("click", function () {
-            pTag.contentEditable = false;
+    pTag.appendChild(btnA);
 
-            /////////////////////////////////////////////////////////////////////////////////////////////////////
-        })){} /////////////////Eventos///////////////////////////////
-        })){}
-       ///////////////////////////////////////////////////////////////////////////////////////////////////////////// 
+    btnA.addEventListener("click", function () {
+        pTag.contentEditable = false;
+        const updatedValue = pTag.innerText;
 
-    } else { if (selection === "Event") {
-        const containerN =document.createElement("div")
-        const pTag = document.createElement("p");
-        pTag.id ="TextoP"
-        const btnD = document.createElement("button");
-        const btnE = document.createElement("button");
-        containerN.appendChild(pTag);
-        containerN.appendChild(btnD);
-        containerN.appendChild(btnE);
+        ///Eliminar el botón "Apply" antes de guardar///
+        pTag.removeChild(btnA);
 
-        ////////////////
-        let evnt = tareaEvent.value +" "+ DateR.value;
-        pTag.innerHTML = evnt
-        ////////////////
-        
-        btnD.innerHTML = "Delete Tasks";
-        btnE.innerHTML = "Edit";
-        listaEvento.appendChild(containerN)
+        if (type === "Task") {
+            taskLista[index] = updatedValue;
+            localStorage.setItem("taskInfo", JSON.stringify(taskLista));
+        } else {
+            eventLista[index] = updatedValue;
+            localStorage.setItem("eventInfo", JSON.stringify(eventLista));
+        }
+    });
+}
 
-        ///////////////
-        taskLista.push (evnt);
-        localStorage.setItem ("eventInfo", JSON.stringify(eventLista))
-        //////////////
-
-        if (btnD.addEventListener("click", function () {
-            listaEvento.removeChild(containerN);
-            alert("Deleted Succesfully");
-        })) {}
-        
-        if (btnE.addEventListener("click", function () {
-            const btnA = document.createElement("button");
-            containerN.appendChild(btnA);
-            btnA.innerHTML = "Apply";
-            pTag.contentEditable = true;
-        
-        if (btnA.addEventListener("click", function () {
-            pTag.contentEditable = false;
-            
-        })){}
-        })){}
-    } }})
+///Renderizar la lista al cargar la página///
+renderList();
